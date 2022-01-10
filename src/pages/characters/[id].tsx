2e89acd5e7803e22d36340d53d13046e869b462f
Felipe Barcelos <felipebarcelospro@gmail.com>
2022-01-10 08:17:26 -0300

    feat: add simple SEO Meta Tags

--- src/pages/characters/[id].tsx
+++ src/pages/characters/[id].tsx
@@ -1,4 +1,5 @@
 import NextImage from 'next/image'
+import NextHead from 'next/head'
 
 import { ComicList } from '../../components/comic-list'
 import { Share } from '../../components/share'
@@ -8,70 +9,76 @@ import { Banner, Container } from '../../styles/pages/characters'
 
 export default function SingleCharacter (): React.ReactElement {
   return (
-    <AppLayout>
-      <Container>
-        <Banner>
+    <>
+      <NextHead>
+        <title>Marvel Hub - Character</title>
+      </NextHead>
+
+      <AppLayout>
+        <Container>
+          <Banner>
+            <Wrapper>
+              <div className='thumbnail-container'>
+                <div className="thumbnail">
+                  <NextImage src='http://i.annihil.us/u/prod/marvel/i/mg/c/e0/535fecbbb9784.jpg' width={350} height={350} />
+                </div>
+              </div>
+              <div className='info-container'>
+                <h1>3-D Man</h1>
+
+                <hr />
+
+                <div className="item">
+                  <h1>Description:</h1>
+                  <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero earum nam assumenda eos, asperiores cupiditate velit ipsam fuga iste sunt rem alias, minus laboriosam neque quia beatae atque. Eos, eaque.</p>
+                </div>
+
+                <br />
+
+                <Share />
+              </div>
+            </Wrapper>
+          </Banner>
+
           <Wrapper>
-            <div className='thumbnail-container'>
-              <div className="thumbnail">
-                <NextImage src='http://i.annihil.us/u/prod/marvel/i/mg/c/e0/535fecbbb9784.jpg' width={350} height={350} />
-              </div>
-            </div>
-            <div className='info-container'>
-              <h1>3-D Man</h1>
-
-              <hr />
-
-              <div className="item">
-                <h1>Description:</h1>
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero earum nam assumenda eos, asperiores cupiditate velit ipsam fuga iste sunt rem alias, minus laboriosam neque quia beatae atque. Eos, eaque.</p>
-              </div>
-
-              <br />
-
-              <Share />
-            </div>
+            <ComicList
+              title="Comics of 3-D Man"
+              data={[
+                {
+                  id: '1',
+                  title: 'Black Widow #13',
+                  cover: 'https://i.annihil.us/u/prod/marvel/i/mg/3/90/61d4c5184d772/portrait_uncanny.jpg',
+                  author: 'Thompson, Pimentel'
+                },
+                {
+                  id: '1',
+                  title: 'The Amazing Spider-Man #84',
+                  cover: 'https://i.annihil.us/u/prod/marvel/i/mg/f/40/61d4c41cded7e/portrait_uncanny.jpg',
+                  author: 'Ziglar, Medina'
+                },
+                {
+                  id: '1',
+                  title: 'Wastelanders: Doom #1',
+                  cover: 'https://i.annihil.us/u/prod/marvel/i/mg/9/70/61d4c517ae86c/portrait_uncanny.jpg',
+                  author: 'Gronbekk, Ohta'
+                },
+                {
+                  id: '1',
+                  title: 'Shang-Chi #7',
+                  cover: 'https://i.annihil.us/u/prod/marvel/i/mg/c/00/61d4c4fa11be9/portrait_uncanny.jpg',
+                  author: 'Yang, Ruan'
+                },
+                {
+                  id: '1',
+                  title: 'Captain Marvel #35',
+                  cover: 'https://i.annihil.us/u/prod/marvel/i/mg/3/d0/61d4c4386681f/portrait_uncanny.jpg',
+                  author: 'Thompson, Davila'
+                }
+              ]}
+            />
           </Wrapper>
-        </Banner>
-
-        <Wrapper>
-          <ComicList
-            title="Comics of 3-D Man"
-            data={[
-              {
-                id: '1',
-                title: 'Black Widow #13',
-                cover: 'https://i.annihil.us/u/prod/marvel/i/mg/3/90/61d4c5184d772/portrait_uncanny.jpg',
-                author: 'Thompson, Pimentel'
-              },
-              {
-                id: '1',
-                title: 'The Amazing Spider-Man #84',
-                cover: 'https://i.annihil.us/u/prod/marvel/i/mg/f/40/61d4c41cded7e/portrait_uncanny.jpg',
-                author: 'Ziglar, Medina'
-              },
-              {
-                id: '1',
-                title: 'Wastelanders: Doom #1',
-                cover: 'https://i.annihil.us/u/prod/marvel/i/mg/9/70/61d4c517ae86c/portrait_uncanny.jpg',
-                author: 'Gronbekk, Ohta'
-              },
-              {
-                id: '1',
-                title: 'Shang-Chi #7',
-                cover: 'https://i.annihil.us/u/prod/marvel/i/mg/c/00/61d4c4fa11be9/portrait_uncanny.jpg',
-                author: 'Yang, Ruan'
-              },
-              {
-                id: '1',
-                title: 'Captain Marvel #35',
-                cover: 'https://i.annihil.us/u/prod/marvel/i/mg/3/d0/61d4c4386681f/portrait_uncanny.jpg',
-                author: 'Thompson, Davila'
-              }
-            ]}
-          />
-        </Wrapper>
-      </Container>
-    </AppLayout>
+        </Container>
+      </AppLayout>
+    </>
   )
 }
--- src/pages/comics/[id].tsx
+++ src/pages/comics/[id].tsx
@@ -1,4 +1,5 @@
 import NextImage from 'next/image'
+import NextHead from 'next/head'
 
 import { ComicList } from '../../components/comic-list'
 import { Share } from '../../components/share'
@@ -8,98 +9,104 @@ import { Container, Banner } from '../../styles/pages/comics'
 
 export default function SingleComic (): React.ReactElement {
   return (
-    <AppLayout>
-      <Container>
-        <Banner>
+    <>
+      <NextHead>
+        <title>Marvel Hub - Comic</title>
+      </NextHead>
+
+      <AppLayout>
+        <Container>
+          <Banner>
+            <Wrapper>
+              <div className='thumbnail-container'>
+                <NextImage src='https://i.annihil.us/u/prod/marvel/i/mg/5/c0/61ae1d0674d31/clean.jpg' width={350} height={540} />
+              </div>
+              <div className='info-container'>
+                <h1>Star Wars: Doctor Aphra (2020) #17</h1>
+
+                <div className="item">
+                  <h1>Published:</h1>
+                  <h2>January 05, 2022</h2>
+                </div>
+
+                <hr />
+
+                <div className="row">
+                  <div className="col-md">
+                    <div className="item">
+                      <h1>Writer:</h1>
+                      <h2>Alyssa Wong</h2>
+                    </div>
+                  </div>
+
+                  <div className="col-md">
+                    <div className="item">
+                      <h1>Penciler:</h1>
+                      <h2>Minkyu Jung</h2>
+                    </div>
+                  </div>
+                </div>
+
+                <hr />
+
+                <div className="item">
+                  <h1>Cover Artist:</h1>
+                  <h2>Sara Pichelli</h2>
+                </div>
+
+                <hr />
+
+                <div className="item">
+                  <h1>Cover Artist:</h1>
+                  <p>EVOCATIONS! DOCTOR APHRA and SANA STARROS stumble upon a STRANGE RITUAL...And STRANGER ENEMY! Will they fall victim to a practitioner of an ANCIENT CULT?</p>
+                </div>
+
+                <br />
+
+                <Share />
+              </div>
+            </Wrapper>
+          </Banner>
+
           <Wrapper>
-            <div className='thumbnail-container'>
-              <NextImage src='https://i.annihil.us/u/prod/marvel/i/mg/5/c0/61ae1d0674d31/clean.jpg' width={350} height={540} />
-            </div>
-            <div className='info-container'>
-              <h1>Star Wars: Doctor Aphra (2020) #17</h1>
-
-              <div className="item">
-                <h1>Published:</h1>
-                <h2>January 05, 2022</h2>
-              </div>
-
-              <hr />
-
-              <div className="row">
-                <div className="col-md">
-                  <div className="item">
-                    <h1>Writer:</h1>
-                    <h2>Alyssa Wong</h2>
-                  </div>
-                </div>
-
-                <div className="col-md">
-                  <div className="item">
-                    <h1>Penciler:</h1>
-                    <h2>Minkyu Jung</h2>
-                  </div>
-                </div>
-              </div>
-
-              <hr />
-
-              <div className="item">
-                <h1>Cover Artist:</h1>
-                <h2>Sara Pichelli</h2>
-              </div>
-
-              <hr />
-
-              <div className="item">
-                <h1>Cover Artist:</h1>
-                <p>EVOCATIONS! DOCTOR APHRA and SANA STARROS stumble upon a STRANGE RITUAL...And STRANGER ENEMY! Will they fall victim to a practitioner of an ANCIENT CULT?</p>
-              </div>
-
-              <br />
-
-              <Share />
-            </div>
+            <ComicList
+              title="Best of Spider Man"
+              data={[
+                {
+                  id: '1',
+                  title: 'Black Widow #13',
+                  cover: 'https://i.annihil.us/u/prod/marvel/i/mg/3/90/61d4c5184d772/portrait_uncanny.jpg',
+                  author: 'Thompson, Pimentel'
+                },
+                {
+                  id: '1',
+                  title: 'The Amazing Spider-Man #84',
+                  cover: 'https://i.annihil.us/u/prod/marvel/i/mg/f/40/61d4c41cded7e/portrait_uncanny.jpg',
+                  author: 'Ziglar, Medina'
+                },
+                {
+                  id: '1',
+                  title: 'Wastelanders: Doom #1',
+                  cover: 'https://i.annihil.us/u/prod/marvel/i/mg/9/70/61d4c517ae86c/portrait_uncanny.jpg',
+                  author: 'Gronbekk, Ohta'
+                },
+                {
+                  id: '1',
+                  title: 'Shang-Chi #7',
+                  cover: 'https://i.annihil.us/u/prod/marvel/i/mg/c/00/61d4c4fa11be9/portrait_uncanny.jpg',
+                  author: 'Yang, Ruan'
+                },
+                {
+                  id: '1',
+                  title: 'Captain Marvel #35',
+                  cover: 'https://i.annihil.us/u/prod/marvel/i/mg/3/d0/61d4c4386681f/portrait_uncanny.jpg',
+                  author: 'Thompson, Davila'
+                }
+              ]}
+            />
           </Wrapper>
-        </Banner>
-
-        <Wrapper>
-          <ComicList
-            title="Best of Spider Man"
-            data={[
-              {
-                id: '1',
-                title: 'Black Widow #13',
-                cover: 'https://i.annihil.us/u/prod/marvel/i/mg/3/90/61d4c5184d772/portrait_uncanny.jpg',
-                author: 'Thompson, Pimentel'
-              },
-              {
-                id: '1',
-                title: 'The Amazing Spider-Man #84',
-                cover: 'https://i.annihil.us/u/prod/marvel/i/mg/f/40/61d4c41cded7e/portrait_uncanny.jpg',
-                author: 'Ziglar, Medina'
-              },
-              {
-                id: '1',
-                title: 'Wastelanders: Doom #1',
-                cover: 'https://i.annihil.us/u/prod/marvel/i/mg/9/70/61d4c517ae86c/portrait_uncanny.jpg',
-                author: 'Gronbekk, Ohta'
-              },
-              {
-                id: '1',
-                title: 'Shang-Chi #7',
-                cover: 'https://i.annihil.us/u/prod/marvel/i/mg/c/00/61d4c4fa11be9/portrait_uncanny.jpg',
-                author: 'Yang, Ruan'
-              },
-              {
-                id: '1',
-                title: 'Captain Marvel #35',
-                cover: 'https://i.annihil.us/u/prod/marvel/i/mg/3/d0/61d4c4386681f/portrait_uncanny.jpg',
-                author: 'Thompson, Davila'
-              }
-            ]}
-          />
-        </Wrapper>
-      </Container>
-    </AppLayout>
+        </Container>
+      </AppLayout>
+    </>
   )
 }
