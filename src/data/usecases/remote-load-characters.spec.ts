fe1817f900869c6bf1c9bc7fd0e03e7c4cedbd0c
Felipe Barcelos <felipebarcelospro@gmail.com>
2022-01-11 18:55:49 -0300

    test: ensure RemoteLoadCharacters calls httpClient with correct URL

--- src/data/usecases/remote-load-characters.spec.ts
+++ src/data/usecases/remote-load-characters.spec.ts
@@ -7,6 +7,19 @@ interface Sut {
   httpClient: HttpClientSpy<MarvelHttpResponse>
 }
 
+const makeFakeCharacter = (): unknown => ({
+  id: '1',
+  name: 'any_name',
+  description: 'any_description',
+  thumbnail: {
+    path: 'any_cover',
+    extension: 'jpg'
+  },
+  comics: {
+    available: 10
+  }
+})
+
 const makeSut = (): Sut => {
   const httpClient = new HttpClientSpy()
   const sut = new RemoteLoadCharactersList(httpClient)
@@ -19,6 +32,24 @@ describe('RemoteLoadCharactersList', () => {
     const { sut } = makeSut()
 
     expect(sut).toBeTruthy()
+  })
+
+  it('should call httpClient with correct URL', async () => {
+    const { sut, httpClient } = makeSut()
+
+    httpClient.response.body = {
+      code: 200,
+      status: 'ok',
+      data: {
+        results: [
+          makeFakeCharacter()
+        ]
+      }
+    }
+
+    await sut.execute()
+
+    expect(httpClient.url).toBe('/characters')
   })
 })
 
