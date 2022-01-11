26681d150809551e33b0a53379c43cd7e2918ba3
Felipe Barcelos <felipebarcelospro@gmail.com>
2022-01-11 19:01:00 -0300

    test: ensure RemoteLoadCharacterById calls httpClient with correct URL

--- src/data/usecases/remote-load-character-by-id.spec.ts
+++ src/data/usecases/remote-load-character-by-id.spec.ts
@@ -7,6 +7,19 @@ interface Sut {
   httpClient: HttpClientSpy<MarvelHttpResponse>
 }
 
+const makeFakeCharacter = (): unknown => ({
+  id: '1',
+  name: 'any_name',
+  description: 'any_description',
+  thumbnail: {
+    path: 'any_cover',
+    extension: 'jpg'
+  },
+  comics: {
+    available: 10
+  }
+})
+
 const makeSut = (): Sut => {
   const httpClient = new HttpClientSpy()
   const sut = new RemoteLoadCharacterById(httpClient)
@@ -19,5 +32,23 @@ describe('RemoteLoadCharacterById', () => {
     const { sut } = makeSut()
 
     expect(sut).toBeTruthy()
+  })
+
+  it('should call httpClient with correct URL', async () => {
+    const { sut, httpClient } = makeSut()
+
+    httpClient.response.body = {
+      code: 200,
+      status: 'ok',
+      data: {
+        results: [
+          makeFakeCharacter()
+        ]
+      }
+    }
+
+    await sut.execute({ id: '1' })
+
+    expect(httpClient.url).toBe('/characters/1')
   })
 })
